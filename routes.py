ec3318273dbdb5a674b3068b07d0e93fb1e626a1
Steffen Devich <devichs4@gmail.com>
2025-05-29 21:10:19 -0500

    Modified routes.py due to inadvertently removing several routes that were in the original file. Copied those out of the commit statement and added them back.

--- routes.py
+++ routes.py
@@ -21,6 +21,75 @@ DB_NAME = "receipts.sqlite" # Or manage_receipt_tables.DB_NAME
 @route('/home')
 @view('index')
 def home():
+    
+    @route('/list')
+    @view('list')
+    def list():
+        con = sqlite3.connect("receipts.sqlite")
+        c = con.cursor()
+        c.execute(""" 
+        select id,store,category,item,quantity,ui,'$' || cast(cost as float) as cost,purchasedate from receipts
+        """)
+
+        result = c.fetchall()
+        output = template("list",rows = result)
+        return output
+   
+    @route("/budget")
+    def  budget():
+        con = sqlite3.connect("receipts.sqlite")
+        c = con.cursor()
+        c.execute("""
+        select distinct '$' || cast(amount as float) as 'amount', date(budgetdate) as 'DateSet','$' || r.cost as 'ExpensesTotal','$' || cast((x.budget - r.cost) as float) as 'WhatsLleft'
+            from budget b,
+            (select sum(cost)cost from receipts where purchasedate >= (select max(date(budgetdate)) from budget))r,
+        (select distinct amount as budget,id  from budget where id in (
+        select max(id) from budget))x
+        where b.id in (
+        select max(id) from budget) 
+        order by b.budgetdate desc	
+        """)
+
+        result = c.fetchall()	
+        output = template("budget",rows = result)   
+        return output
+    
+    @route("/new",method = "GET")
+    def new_item(): 
+        if request.GET.get("save","").strip():
+            newStore = request.GET.get("store","").strip()
+            newCategory = request.GET.get("category","").strip()
+            newItem = request.GET.get("item","").strip()
+            newQuantity = request.GET.get("quantity","").strip()
+            newUi = request.GET.get("ui","").strip()
+            newCost = request.GET.get("cost","").strip()
+            newPurchasedate = request.GET.get("purchasedate","").strip()
+            con = sqlite3.connect("receipts.sqlite")
+            c = con.cursor()
+            c.execute("""
+            insert into receipts(store,category,item,quantity,ui,cost,purchasedate)values(?,?,?,?,?,?,?)""",(newStore,newCategory,newItem,newQuantity,newUi,newCost,newPurchasedate,))
+            new_id = c.lastrowid 
+            con.commit()
+            c.close()
+            url = "http://localhost:5555/list"
+            return"<p>The new receipt was inserted into the database, the ID is {0} </p><p><a href = http://localhost:5555/list>List</a></p><p><a href = http://localhost:5555/new>New Expense</a></p>".format(new_id)
+        else:
+            return template("new_receipt.tpl")    
+    
+    @route("/newbudget",method = "GET") 
+    def new_budget():
+        if request.GET.get("update","").strip():
+            newAmount = request.GET.get("amount","").strip()
+            con = sqlite3.connect("receipts.sqlite")
+            c = con.cursor()
+            c.execute("""
+            insert into budget(amount)values(?)""",(newAmount,))
+            new_id = c.lastrowid 
+            con.commit()
+            c.close()
+            return"<p>The new budget amount was inserted into the database, the ID is {0} </p></p><a href = http://localhost:5555/budget>Budget</a>".format(new_id)
+        else:    
+            return template("new_budget.tpl")
 
     @route('/import_transactions', method='GET')
     def show_import_form():
